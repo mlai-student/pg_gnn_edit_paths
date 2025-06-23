b3cc1b81930f32bb5df9bf9f915db6839930af6f
florian <florian.seiffarth@t-online.de>
2025-06-23 21:46:20 +0200

    added timeout

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ if __name__ == '__main__':
     )
     # create the nx_graphs from the pytorch geometric dataset
     graph_dataset.create_nx_graphs()
+    # print the maximum runtime for the generation of the edit paths
+    print(f"Generating pairwise edit paths for the {db_name} dataset with the following parameters: {edit_path_generation_parameters}")
+    # print the number of graphs in the dataset
+    print(f"Number of graphs in the {db_name} dataset: {len(graph_dataset)}")
+    # print the maximum time
+    print(f"Maximum runtime: {round(edit_path_generation_parameters['timeout'] * len(graph_dataset) * (len(graph_dataset) - 1) / 2 / (60 * 60), 2)} hours")
     print(f"Loaded MUTAG dataset with {len(graph_dataset)} graphs")
     # generate the pairwise edit paths (not optimal) and save them to a file
     generate_pairwise_edit_paths(graph_dataset,
